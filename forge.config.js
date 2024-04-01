3bc7f72afec0ec8f4e327744d367937cd30219cc
songjx <jinxia.song@siemens.com>
2024-04-01 14:25:13 +0800

    remove the token

--- forge.config.js
+++ forge.config.js
@@ -55,7 +55,7 @@ module.exports = {
         owner: 'idSong',
         name: 'electron-hello-sample'
       },
-      authToken: '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',
+      // authToken: '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',
       // authToken: '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', 
       prerelease: true,
       draft: true
